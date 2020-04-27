2a5af4b49958af597ebb78cd7cc4c5ebef1caf62
kimsaan <wlstks7@naver.com>
2020-04-27 16:50:02 +0000

    upload

---
+++ image-link.js
@@ -0,0 +1,260 @@
+function kings_info(){
+	var rank = 0;
+	var nick = '';
+	var name = '';
+	var lifetime = '';
+	var onthetop = '';
+	var cycle = '';
+	var upage = '';
+	var img_url = '';
+	var comment = '';
+	var copyrights = '';
+	var copyrights_url = '';
+	var foryou = '';
+}
+
+	var kings = new Array(27);
+	
+	for(var i = 0; i < 27; i++)
+		kings[i] = new kings_info();
+	
+	kings[0].rank = 1;
+	kings[0].nick = '태조';
+	kings[0].name = '이성계';
+	kings[0].lifetime = '1335~1408년 (태종 8년)';
+	kings[0].onthetop = '1392~1398년';
+	kings[0].cycle = 6;
+	kings[0].upage = 58;
+	kings[0].img_url = 'https://ww.namu.la/s/9d526f8f65545f83bc7384e7b7187802647d2b210403cdee92bf2e163a787104c5e419992fdb60569fef21ee31d7f1e74624360447c653fb5e108b53c069f7b569e045566a4bae18e3dab1a704db3f61d939583106c374a32dbbb464e12341a824f3398119acf484fca83f8a9e5ac5b2';
+	kings[0].comment = '';
+	kings[0].copyrights = '나무위키';
+	kings[0].copyrights_url = 'https://namu.wiki/w/%ED%83%9C%EC%A1%B0(%EC%A1%B0%EC%84%A0)';
+	kings[0].foryou = '화살이 과녁을 찾아가는 것이 아니라, 활 쏘는 이가 과녁으로 화살을 보내야한다. <태조실록>중.</br> 역대 왕중 최고의 신궁으로 꼽히며, 백 보 밖에 있는 배나무의 배를 활로 맞추었다는 일화에서 화살의 역할 보다 활을 소는 이의 역할과 집중력이 중요하다고 말했다. 이런 지혜와 집중력이 조선을 건국할 수 있었던 것 아닐가 싶다.</br> 당신은 태조처럼 이런 지혜와 집중력을 가지셨군요!';
+	
+	kings[1].rank = 2;
+	kings[1].nick = '정종';
+	kings[1].name = '이방과';
+	kings[1].lifetime = '1357~1419년 (세종 1년)';
+	kings[1].onthetop = '1398~1400년';
+	kings[1].cycle = 2;
+	kings[1].upage = 42;
+	kings[1].comment = '';
+	kings[1].copyrights = '';
+	kings[1].copyrights_url = '';
+	kings[1].foryou = '과인이 병이 있어 수족이 저리고 아프니, 때때로 격구를 하여 몸을 움직여서 기운을 통하게 하려고 한다. <정조실록>중.</br> 정종은 왕이었지만, 실권자가 아니었다. 왕자의 난을 일으킨 동생 이방원(태종)으로 인해 정종은 스스로 몸을 낮출 수밖에 없었다.  정종은 매일 격구를 하여 동생에게 정치나 권력에 사심이 없음을 이렇게 내비추었다. 동생과의 술자리에서 춤을 추는 등 자유를 원했을 지도 모른다. 왕이 아닌 정종의 삶은 영리했다고 볼 수 있다. </br> 당신은 정종처럼 자신의 상황을 잘 파악하고 행동할 수 있는 처세술을 가지신 분이군요. 어떤 상황에서든 당신은 자신의 자리를 찾아내실 수 있습니다!';
+	
+	kings[2].rank = 3;
+	kings[2].nick = '태종';
+	kings[2].name = '이방원';
+	kings[2].lifetime = '1367~1422년(세종 4년)';
+	kings[2].onthetop = '1400-1418년';
+	kings[2].cycle = 18;
+	kings[2].upage = 34;
+	kings[2].comment = '';
+	kings[2].copyrights = '나무위키';
+	kings[2].copyrights_url = 'https://w.namu.la/s/0ba0929f53b0b940042a9e8c1daed88aaab9bc55aa002d171f62838506c8df9a41fea90131b01c9504e4aee7cea5559f468a7d225a354725609fde90c9a4e22c02f06956f66c945e4698cc572bde684986654c34f2c6279ce7048ec9b3e7d3965932a4d896e08ce78c69a8d8283e4baf';
+	kings[2].foryou = '태조의 5번째 아들, 형제들을 죽이고 정종의 양위를 받아 즉위하였다. 18년 동안 호랑이를 탔으니, 또한 이미 족하다. <태종실록>중. </br> 1418년, 세종에게 물려주며 위와 같은 말을 했다. 27명의 왕중 유일하게 본인이 양위를 한 임금이다. 피로 물든 왕의 자리였지만, 자신이 물러나야 할 때를 정확히 알고 물러났다. 또한, 태종이 잘 닦아 높은 왕권으로 성군 세종을 탄생시키고 세종이 위대한 업적으로 쌓을 수 있는 자리를 만들어준 일등공신이다. </br> 태종과 같은 당신은 영리하고 의자가 굳건하여 목적을 위해서라면 수단과 방법을 가리지 않는다. 조..금 무섭지만.. 목적은 이루셨나요..?';
+	
+	kings[3].rank = 4;
+	kings[3].nick = '세종';
+	kings[3].name = '이도';
+	kings[3].lifetime = '1397(태조 6년)~1450년(세종 32년)';
+	kings[3].onthetop = '1418~1450년';
+	kings[3].cycle = 32;
+	kings[3].upage = 22;
+	kings[3].comment = '';
+	kings[3].copyrights = '';
+	kings[3].copyrights_url = '';
+	kings[3].foryou = '그대의 자질은 아름답다. 그런 자실을 가지고 아무것도 하지 않겠다 해도 뭐라 할 수는 없지만, 그대가 만약 온 마음과 힘을 다해 노력한다면 무슨 일인들 해내지 못하겠는가. <세종실록>중. </br> 세종은 유능한 신하가 나라에 있어야 나라가 굳건할 수 있다고 믿던 왕이었다. 일 잘하는 신하의 사직서는 찢어 버리며, 한 말이라고 합니다. 조금 무서운 보스네요...(닥치고 일해...)<br/>세종처럼 당신은 워커홀릭은 아니신가요...무엇이든 해낼 수 있다는 믿음과 주변 환경과 인력을 잘 활용하는 당신은 유능한 리더가 될 수 있을껍니다!(조별과제에서 팀원을 힘들게 하지않으셨을까 싶은데...)';
+	
+	kings[4].rank = 5;
+	kings[4].nick = '문종';
+	kings[4].name = '이향';
+	kings[4].lifetime = '1414(세종 23년)~1452년(문종 2년)';
+	kings[4].onthetop = '1450~1452년';
+	kings[4].cycle = 2;
+	kings[4].upage = 37;
+	kings[4].comment = '';
+	kings[4].copyrights = '';
+	kings[4].copyrights_url = '';
+	kings[4].foryou = '나는 더 이상 가망이 없는 것 같소. 홀로 남겨진 세자(단종)를 그대들이 잘 지켜주시오. <문종실록>중. </br> 문종은 아버지 세종을 닮아 어려서부터 학문에 뛰어나고 몸가짐이 정갈하였으나 체력 또한 아버지를 닮아 허약했다. 라는 말과 달리 폐빈된 부인과 단종의 어머니 외에도 8명의 후궁이 있었고 적자 까지 포함하여 3남 5녀가 있었다. </br> ';
+	
+	kings[5].rank = 6;
+	kings[5].nick = '단종';
+	kings[5].name = '이홍위';
+	kings[5].lifetime = '1441(세종 24년)~1457년(세조 3년)';
+	kings[5].onthetop = '1452~1455년';
+	kings[5].cycle = 3;
+	kings[5].upage = 12;
+	
+	kings[6].rank = 7;
+	kings[6].nick = '세조';
+	kings[6].name = '이유';
+	kings[6].lifetime = '1417(태종 17년)~1468년(세조 13)';
+	kings[6].onthetop = '1455~1468년';
+	kings[6].cycle = 13;
+	kings[6].upage = 39;
+	
+	kings[7].rank = 8;
+	kings[7].nick = '예종';
+	kings[7].name = '이황';
+	kings[7].lifetime = '1450(세종 32년)~1469년(예종 1년)';
+	kings[7].onthetop = '1468~1469년';
+	kings[7].cycle = 1;
+	kings[7].upage = 19;
+	
+	kings[8].rank = 9;
+	kings[8].nick = '성종';
+	kings[8].name = '이혈';
+	kings[8].lifetime = '1457(세조 3년)~1494년(성종 25년)';
+	kings[8].onthetop = '1469~1494년';
+	kings[8].cycle = 25;
+	kings[8].upage = 13;
+	
+	kings[9].rank = 10;
+	kings[9].nick = '연산군';
+	kings[9].name = '이융';
+	kings[9].lifetime = '1476(성종 7)~1506년(중종 1)';
+	kings[9].onthetop = '1494~1506년';
+	kings[9].cycle = 25;
+	kings[9].upage = 13;
+	
+	kings[10].rank = 11;
+	kings[10].nick = '중종';
+	kings[10].name = '이역';
+	kings[10].lifetime = '1488(성종 19년)~1544년(중종 38년)';
+	kings[10].onthetop = '1506~1544년';
+	kings[10].cycle = 38;
+	kings[10].upage = 19;
+	
+	kings[11].rank = 12;
+	kings[11].nick = '인종';
+	kings[11].name = '이호';
+	kings[11].lifetime = '1515(중종 10년)~1545년(인종 1년)';
+	kings[11].onthetop = '1544~1545';
+	kings[11].cycle = 1;
+	kings[11].upage = 30;
+	
+	kings[12].rank = 13;
+	kings[12].nick = '명종';
+	kings[12].name = '이환';
+	kings[12].lifetime = '1534(중종 29년)~1567년(명종 22)';
+	kings[12].onthetop = '1545~1567년';
+	kings[12].cycle = 22;
+	kings[12].upage = 12;
+	
+	kings[13].rank = 14;
+	kings[13].nick = '선조';
+	kings[13].name = '이연';
+	kings[13].lifetime = '1522(명종 7년)~1608년(선조 41년)';
+	kings[13].onthetop = '1567~1608년';
+	kings[13].cycle = 41;
+	kings[13].upage = 16;
+	
+	kings[14].rank = 15;
+	kings[14].nick = '광해군';
+	kings[14].name = '이혼';
+	kings[14].lifetime = '1575(선조 8년)~1641년(인조 19)';
+	kings[14].onthetop = '1608~1623년';
+	kings[14].cycle = 15;
+	kings[14].upage = 34;
+	
+	kings[15].rank = 16;
+	kings[15].nick = '인조';
+	kings[15].name = '이종';
+	kings[15].lifetime = '1595(광해군 11년)~1659년(효종 10년)';
+	kings[15].onthetop = '1623~1649년';
+	kings[15].cycle = 26;
+	kings[15].upage = 29;
+	
+	kings[16].rank = 17;
+	kings[16].nick = '효종';
+	kings[16].name = '이호';
+	kings[16].lifetime = '1619(광해군 11년)~1659년(효종 10년)';
+	kings[16].onthetop = '1649~1659년';
+	kings[16].cycle = 10;
+	kings[16].upage = 31;
+	
+	kings[17].rank = 18;
+	kings[17].nick = '현종';
+	kings[17].name = '이연';
+	kings[17].lifetime = '1641(인조 19년)~1674년(현종 15년)';
+	kings[17].onthetop = '1659~1674년';
+	kings[17].cycle = 15;
+	kings[17].upage = 19;
+	
+	kings[18].rank = 19;
+	kings[18].nick = '숙종';
+	kings[18].name = '이순';
+	kings[18].lifetime = '1661(현종 2년)~1720년(숙종 46년)';
+	kings[18].onthetop = '1674~1720년';
+	kings[18].cycle = 46;
+	kings[18].upage = 14;
+	
+	kings[19].rank = 20;
+	kings[19].nick = '경종';
+	kings[19].name = '이윤';
+	kings[19].lifetime = '1688(숙종 14년)~1724년(경종 4년)';
+	kings[19].onthetop = '1720~1724년';
+	kings[19].cycle = 4;
+	kings[19].upage = 33;
+	
+	kings[20].rank = 21;
+	kings[20].nick = '영조';
+	kings[20].name = '이금';
+	kings[20].lifetime = '1694(숙종 20년)~1776년(영조 52년)'; 
+	kings[20].onthetop = '1724~1776년';
+	kings[20].cycle = 52;
+	kings[20].upage = 31;
+	
+	kings[21].rank = 22;
+	kings[21].nick = '정조';
+	kings[21].name = '이산';
+	kings[21].lifetime = '1752(영조 28년)~1800년(정조 24년)'; 
+	kings[21].onthetop = '1776~1800년';
+	kings[21].cycle = 24;
+	kings[21].upage = 25;
+	kings[21].comment = '';
+	kings[21].copyrights = '<a href="https://namu.wiki/w/'+kings[21].nick+'(조선)"  alt="링크" target="_blank">'+kings[21].nick+'를 더 알아보자(나무위키)</a>';
+	kings[21].copyrights_url = 'https://ww.namu.la/s/9ca3870a5d1a700e2c315a5d77a4830ad9c5e52e367d503e66688b75f4620d64144404a92e279151fcb58cb57d602c8f9a4820ab720e144cef66e1099ab1df8e628f3f3aa356c5e77018f313adb83424d91b89306657657048db8b9d815470dd';
+	kings[21].foryou = '뒤주에 같혀보고 싶냐?!!';
+	
+	kings[22].rank = 23;
+	kings[22].nick = '순조';
+	kings[22].name = '이공';
+	kings[22].lifetime = '1790(정조 14년)~1834년(순조 34년)'; 
+	kings[22].onthetop = '1800~1834년';
+	kings[22].cycle = 34;
+	kings[22].upage = 11;
+	
+	kings[23].rank = 24;
+	kings[23].nick = '헌종';
+	kings[23].name = '이환';
+	kings[23].lifetime = '1827(순조 27년)~1849년(헌종 15년)'; 
+	kings[23].onthetop = '1834~1849년';
+	kings[23].cycle = 15;
+	kings[23].upage = 8;
+	
+	kings[24].rank = 25;
+	kings[24].nick = '철종';
+	kings[24].name = '이원범';
+	kings[24].lifetime = '1831(순조 31년)~1863년(철종 14년)'; 
+	kings[24].onthetop = '1849~1863년';
+	kings[24].cycle = 14;
+	kings[24].upage = 19;
+	
+	kings[25].rank = 26;
+	kings[25].nick = '고종';
+	kings[25].name = '이명복';
+	kings[25].lifetime = '1852(철종 3)~1919년'; 
+	kings[25].onthetop = '1862~1907년';
+	kings[25].cycle = 44;
+	kings[25].upage = 12;
+	
+	kings[26].rank = 27;
+	kings[26].nick = '순종';
+	kings[26].name = '이척';
+	kings[26].lifetime = '1874(고종 11년)~1926년'; 
+	kings[26].onthetop = '1907~1910년';
+	kings[26].cycle = 3;
+	kings[26].upage = 34;
